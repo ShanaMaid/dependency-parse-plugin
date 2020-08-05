a9b07799cef990443d4676187b5029fe41673545
shanamaid <uestczeng@gmail.com>
2020-08-05 17:26:18 +0800

    chore: 完成插件

---
+++ index.js
@@ -0,0 +1,88 @@
+let PluginTitle = 'DependencyParsePlugin';
+const fs =require('fs');
+
+
+class DependencyParsePlugin {
+  // 每个文件的直接依赖
+  directDeps = {};
+
+  // 每个文件的所有依赖
+  allDeps = {};
+
+  options = {
+    includes: [],
+    excludes: [],
+  };
+
+  constructor(options) {
+    this.options = Object.assign({}, this.options, options);
+  }
+
+  parseOneFileAllDependency = (filePath) => {
+    const result = {};
+    let i = 0;
+    const len = Object.keys(this.directDeps).length;
+    const dfs = (filePath) => {
+      console.log(`${i++} / ${len}`)
+      const temp = this.directDeps[filePath] || [];
+      const deps = [];
+      deps.push(...temp);
+      temp.forEach((_) => {
+        console.log(_)
+        if (this.allDeps[_]) {
+          deps.push(...this.allDeps[_]);
+        } else {
+          deps.push(...dfs(_));
+        }
+      });
+      this.allDeps[filePath] = deps;
+      return deps;
+    };
+    dfs(filePath);
+  };
+
+  parseModule = (module) => {
+    if (!module || !module.resource || this.directDeps[module.resource]) {
+      return;
+    }
+    const { includes, excludes }  = this.options;
+    if (includes.some((_) => module.resource.match(_))) {
+      
+    } else if (excludes.some((_) => module.resource.match(_))) {
+      return;
+    }
+    this.directDeps[module.resource] = [];
+    (module.dependencies || []).forEach((_) => {
+      const r  = this.parseModule(_.module);
+      if (r) {
+        this.directDeps[module.resource].push(r);
+      }
+    });
+  
+    return module.resource;
+  };
+
+  apply(compiler) {
+    // 解析依赖树
+    compiler.hooks.compilation.tap(PluginTitle, (compilation) => {
+      compilation.hooks.optimizeModules.tap(PluginTitle, (modules) => {
+        modules.forEach(this.parseModule)
+      });
+      
+    });
+
+    // 编译完成，回抛依赖树
+    compiler.hooks.done.tap(PluginTitle, () => {
+      Object.keys(this.directDeps).forEach(this.parseOneFileAllDependency);
+      const { callback } = this.options;
+      if (callback) {
+        callback({
+          directDeps: this.directDeps,
+          allDeps: this.allDeps,
+        });
+      }
+    });
+  }
+}
+
+module.exports = DependencyParsePlugin;
