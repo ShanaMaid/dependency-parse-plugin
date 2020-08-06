0631f71c0fa516b30cdde57b6c7e0f43322992c7
shanamaid <uestczeng@gmail.com>
2020-08-06 16:36:21 +0800

    chore: 优化解析逻辑

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 let PluginTitle = 'DependencyParsePlugin';
 
+
+const regArrMatch = (str, regArr) => regArr.some((_) => str.match(_));
+const regMathArr = (reg, Arr) => Arr.some((_) => _.match(reg));
+
+const getModuleResource = (_) => {
+  if (!_) {
+    return;
+  }
+  if (_.resource) {
+    return _.resource;
+  } else if (_.module) {
+    return getModuleResource(_.module);
+  }
+  return;
+}
+
 class DependencyParsePlugin {
   // 每个文件的直接依赖
   directDeps = {};
@@ -10,6 +26,15 @@ class DependencyParsePlugin {
   options = {
     includes: [],
     excludes: [],
+    /**
+     * {
+     *  name: 'react',
+     *  ignore: [],
+     *  blackList: [], // suport reg
+     *  whiteList: [], // suport reg
+     * }
+     */
+    detectDependences: [],
   };
 
   constructor(options) {
@@ -28,38 +53,105 @@ class DependencyParsePlugin {
           deps.push(...dfs(_));
         }
       });
-      this.allDeps[filePath] = deps;
+      this.allDeps[filePath] = Array.from(new Set(deps));
       return deps;
     };
     dfs(filePath);
   };
 
   parseModule = (module) => {
-    if (!module || !module.resource || this.directDeps[module.resource]) {
-      return;
+    let resource = module && module.resource;
+    if (!resource || this.directDeps[resource]) {
+      return resource;
     }
+   
     const { includes, excludes }  = this.options;
-    if (includes.some((_) => module.resource.match(_))) {
+    if (includes.some((_) => resource.match(_))) {
       
-    } else if (excludes.some((_) => module.resource.match(_))) {
-      return;
+    } else if (excludes.some((_) => resource.match(_))) {
+      return resource;
     }
-    this.directDeps[module.resource] = [];
+    this.directDeps[resource] = [];
     (module.dependencies || []).forEach((_) => {
+      
       const r  = this.parseModule(_.module);
+     
       if (r) {
-        this.directDeps[module.resource].push(r);
+        this.directDeps[resource].push(r);
       }
     });
   
-    return module.resource;
+    return resource;
+  };
+
+  parseTargetDependencies = () => {
+    const { allDeps, directDeps, options } = this;
+    const { detectDependences } = options;
+    const filePaths = Object.keys(allDeps);
+    const importPaths = [];
+
+    detectDependences.forEach((item) => {
+      const { name, blackList = [], whiteList = [], ignores = ['node_modules']} = item;
+      const tempSet = new Set();
+
+      filePaths.forEach((file) => {
+        if (regArrMatch(file, ignores)) {
+          return;
+        }
+        if (regArrMatch(file, whiteList)) {
+          
+        } else if (regArrMatch(file, blackList)) {
+          return;
+        }
+        const values = allDeps[file];
+        values.some((v) => {
+          if (v.match(name) && regMathArr(name, directDeps[file])) {
+            tempSet.add(file);
+            return true;
+          }
+        });
+      });
+    
+      // 对引用路径进行解析
+      const parseImportPaths = (set) => {
+        const result = [];
+        const arr = Array.from(set);
+        const dfs = (currentPath, paths = [currentPath]) => {
+          let hasParent = false;
+          Object.keys(allDeps).forEach((_) => {
+            const v = allDeps[_];
+            if (v.includes(currentPath)) {
+              hasParent = true;
+              const temp = [...paths, _];
+              dfs(_, temp);
+            }
+          });
+          if (!hasParent) {
+            result.push(paths);
+          }
+        };
+        arr.forEach((_) => dfs(_));
+        return result;
+      };
+
+      importPaths.push({
+        name,
+        paths: parseImportPaths(tempSet),
+      })
+    });
+
+    return importPaths;
   };
 
   apply(compiler) {
     // 解析依赖树
     compiler.hooks.compilation.tap(PluginTitle, (compilation) => {
       compilation.hooks.optimizeModules.tap(PluginTitle, (modules) => {
-        modules.forEach(this.parseModule)
+        modules.forEach(this.parseModule);
+        // 对解析完的依赖树进行过滤-去重
+        Object.keys(this.directDeps).forEach((key) => {
+          this.directDeps[key] = Array.from(new Set(this.directDeps[key].filter((_) => _ != key)));
+        })
       });
       
     });
@@ -67,12 +159,16 @@ class DependencyParsePlugin {
     // 编译完成，回抛依赖树
     compiler.hooks.done.tap(PluginTitle, () => {
       Object.keys(this.directDeps).forEach(this.parseOneFileAllDependency);
-      const { callback } = this.options;
+      const { callback, detectDependences } = this.options;
       if (callback) {
-        callback({
+        const params = {
           directDeps: this.directDeps,
           allDeps: this.allDeps,
-        });
+        };
+        if (detectDependences && detectDependences.length) {
+          params.detectDependences = this.parseTargetDependencies();
+        }
+        callback(params);
       }
     });
   }
